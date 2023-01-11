6c6b463473806cec4fa89c1e9c87e58bc8f7a791
Oscar <oscar1149412@gmail.com>
2023-01-10 19:33:54 -0500

    added getlatestreslt method

--- src/main/java/frc/robot/vision/AprilTagHelper.java
+++ src/main/java/frc/robot/vision/AprilTagHelper.java
@@ -1,5 +1,12 @@
 package frc.robot.vision;
 
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
+
 public class AprilTagHelper {
-    
+    public static final PhotonCamera cam = new PhotonCamera("camera");  
+
+    public static PhotonPipelineResult getLatestResult(){
+        return cam.getLatestResult();
+    }
 }
