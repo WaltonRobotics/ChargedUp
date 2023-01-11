f3f3143b5ab0d3509d7e74804ad702b234e1158d
nikki <106131029+philosophermurr@users.noreply.github.com>
2023-01-10 20:11:10 -0500

    added getbesttarget

--- src/main/java/frc/robot/vision/AprilTagHelper.java
+++ src/main/java/frc/robot/vision/AprilTagHelper.java
@@ -2,11 +2,17 @@ package frc.robot.vision;
 
 import org.photonvision.PhotonCamera;
 import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
 
 public class AprilTagHelper {
     public static final PhotonCamera cam = new PhotonCamera("camera");  
 
     public static PhotonPipelineResult getLatestResult(){
         return cam.getLatestResult();
+    }
+
+    public static PhotonTrackedTarget  getBestTarget() {
+        PhotonPipelineResult result = getLatestResult();
+        return result.getBestTarget();
     }
 }
