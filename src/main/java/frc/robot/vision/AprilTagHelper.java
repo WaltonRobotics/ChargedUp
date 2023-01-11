70940c25f7f834a8e3529fe5f0b0d7d60d8b7f2f
Oscar <oscar1149412@gmail.com>
2023-01-10 20:58:01 -0500

    added 3d pose

--- src/main/java/frc/robot/vision/AprilTagHelper.java
+++ src/main/java/frc/robot/vision/AprilTagHelper.java
@@ -1,11 +1,53 @@
 package frc.robot.vision;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Optional;
+
 import org.photonvision.PhotonCamera;
+import org.photonvision.RobotPoseEstimator;
+import org.photonvision.RobotPoseEstimator.PoseStrategy;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.Pair;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.wpilibj.Timer;
+
 public class AprilTagHelper {
-    public static final PhotonCamera cam = new PhotonCamera("camera");  
+    public static final PhotonCamera cam = new PhotonCamera("camera");
+    //distance from robot to camera
+    Transform3d robotToCam = new Transform3d(
+        new Translation3d(0.5, 0.0, 0.5), 
+        new Rotation3d(0,0,0)
+        );
+        //No ChargedUp field?
+        AprilTagFieldLayout aprilTagFieldLayout;
+        ArrayList<Pair<PhotonCamera, Transform3d>> camList = new ArrayList<Pair<PhotonCamera, Transform3d>>();
+        RobotPoseEstimator robotPoseEstimator; 
+
+    public AprilTagHelper(){
+        init();
+        
+    }
+    
+    private void init(){
+        try {
+            aprilTagFieldLayout =  AprilTagFieldLayout.loadFromResource(AprilTagFields.k2022RapidReact.m_resourceFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+            //TODO: write warning statemnt
+        } 
+
+        camList.add(new Pair<PhotonCamera, Transform3d>(cam, robotToCam));
+        robotPoseEstimator = new RobotPoseEstimator(aprilTagFieldLayout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, camList);
+    }
 
     public static PhotonPipelineResult getLatestResult(){
         return cam.getLatestResult();
@@ -14,5 +56,17 @@ public class AprilTagHelper {
     public static PhotonTrackedTarget  getBestTarget() {
         PhotonPipelineResult result = getLatestResult();
         return result.getBestTarget();
+    }
+    
+    public Pair<Pose2d, Double> getEstimatedGlobalPose(Pose2d prevEstimatedRobotPose) {
+        robotPoseEstimator.setReferencePose(prevEstimatedRobotPose);
+    
+        double currentTime = Timer.getFPGATimestamp();
+        Optional<Pair<Pose3d, Double>> result = robotPoseEstimator.update();
+        if (result.isPresent()) {
+            return new Pair<Pose2d, Double>(result.get().getFirst().toPose2d(), currentTime - result.get().getSecond());
+        } else {
+            return new Pair<Pose2d, Double>(null, 0.0);
+        }
     }
 }
