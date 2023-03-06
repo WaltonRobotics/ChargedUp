3ce29464a7501cff390fff3acccd72dba678df8a
graciexie <xiegrace61@gmail.com>
2023-03-06 14:09:19 -0500

    edited ending pts
    Co-Authored-By: nikki <106131029+philosophermurr@users.noreply.github.com>

---
+++ src/main/java/frc/robot/auton/SequentialAutonCommand.java
@@ -0,0 +1,23 @@
+package frc.robot.auton;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+
+public class SequentialAutonCommand extends CommandBase implements Command {
+
+    private Set<CommandBase> m_commandsToUse = new HashSet<>();
+
+    public SequentialAutonCommand(Set<CommandBase> commands) {
+
+    }
+   
+    // @Override
+    // public Set<Subsystem> getRequirements() {
+    //     // TODO Auto-generated method stub
+    //     return null;
+    // } 
+}
