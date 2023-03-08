d9f24de49231b653ba789c6d9ab6fbacfc4ed110
nikki <106131029+philosophermurr@users.noreply.github.com>
2023-03-07 20:40:44 -0500

    diskbrake chooser

--- src/main/java/frc/robot/subsystems/TiltSubsystem.java
+++ src/main/java/frc/robot/subsystems/TiltSubsystem.java
@@ -44,6 +44,7 @@ public class TiltSubsystem extends SubsystemBase {
 	private final GenericEntry nte_actualAngle = DashboardManager.addTabNumberBar(this, "ActualAngle", 0, 35);
 	private final GenericEntry nte_rawAbsVal = DashboardManager.addTabNumberBar(this, "RawAbs", 0, 1);
 	private final GenericEntry nte_coast = DashboardManager.addTabBooleanToggle(this, "coast");
+	private final GenericEntry nte_diskBrake = DashboardManager.addTabBooleanToggle(this, "diskbrake");
 	private final GenericEntry nte_homeSwitch = DashboardManager.addTabBooleanBox(this, "HomeSwitch");
 	private final GenericEntry nte_forwardLimit = DashboardManager.addTabBooleanBox(this, "forward limit");
 
@@ -201,12 +202,17 @@ public class TiltSubsystem extends SubsystemBase {
 		m_motor.setIdleMode(coast ? IdleMode.kCoast : IdleMode.kBrake);
 	}
 
+	private void setDiskBrake(boolean diskBrake) {
+		m_diskBrake.set(diskBrake);
+	}
+
 	@Override
 	public void periodic() {
 		if (!m_homeSwitch.get()) {
 			m_absoluteEncoder.reset();
 		}
 		setCoast(nte_coast.getBoolean(false));
+		setDiskBrake(nte_coast.getBoolean(true));
 		updateShuffleBoard();
 	}
 
