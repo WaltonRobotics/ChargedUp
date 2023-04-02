452d773cf4f1c084b173e497166534aab2ddf496
Banks T <btrout.dhrs@gmail.com>
2023-04-02 01:07:28 +0000

    add PhotonRunner

--- src/main/java/frc/robot/vision/AprilTagCamera.java
+++ src/main/java/frc/robot/vision/AprilTagCamera.java
@@ -3,12 +3,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Optional;
 
-import frc.lib.vision.EstimatedRobotPose;
-// import org.photonvision.EstimatedRobotPose;
+import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
-import frc.lib.vision.PhotonPoseEstimator;
-// import org.photonvision.PhotonPoseEstimator;
-import frc.lib.vision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose2d;
@@ -19,30 +18,43 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 
 public class AprilTagCamera {
-    public final PhotonCamera rightLowCam = new PhotonCamera("RightCornerLow");
-    public final PhotonCamera leftLowCam = new PhotonCamera("LeftCornerLow");
 
     private final Transform3d rightLowRobotToCamera = new Transform3d(
         new Translation3d(Units.inchesToMeters(9.52), Units.inchesToMeters(-9.279), Units.inchesToMeters(8.845)),
         new Rotation3d(0, Units.degreesToRadians(-14), Units.degreesToRadians(39.6)));
-    // private final Transform3d rightLowCameraToRobot = rightLowRobotToCamera.inverse();
 
     private final Transform3d leftLowRobotToCamera = new Transform3d(
         new Translation3d(Units.inchesToMeters(9.52), Units.inchesToMeters(9.279), Units.inchesToMeters(8.845)),
         new Rotation3d(0, Units.degreesToRadians(-14), Units.degreesToRadians(-47.5)));
-    // private final Transform3d leftLowCameraToRobot = leftLowRobotToCamera.inverse();
     
     AprilTagFieldLayout aprilTagFieldLayout;
-    PhotonPoseEstimator leftLowPoseEstimator;
-    PhotonPoseEstimator rightLowPoseEstimator;
 
     // private final DoubleArrayPublisher log_leftRobotToCam, log_rightRobotToCam;
 
+    public final PhotonRunner rightLowCamRunner, leftLowCamRunner;
+
     public AprilTagCamera() {
-        init();
+        try {
+            aprilTagFieldLayout = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2023ChargedUp.m_resourceFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        rightLowCamRunner = new PhotonRunner(
+            "RightCornerLow",
+            rightLowRobotToCamera,
+            aprilTagFieldLayout,
+            () -> new Pose2d()
+        );
+
+        leftLowCamRunner = new PhotonRunner(
+            "RightCornerLow",
+            rightLowRobotToCamera,
+            aprilTagFieldLayout,
+            () -> new Pose2d()
+        );
+
         PhotonCamera.setVersionCheckEnabled(false);
-        // log_leftRobotToCam = WaltLogger.makeDoubleArrTracePub("LeftLowCam_RobotToCam");
-        // log_rightRobotToCam = WaltLogger.makeDoubleArrTracePub("RightLowCam_RobotToCam");
     }
 
     public void updateField2d(Field2d field) {
@@ -54,61 +66,9 @@ public class AprilTagCamera {
         field.getObject("April Tags").setPoses(poseList);
     }
 
-    private void init() {
-        try {
-            aprilTagFieldLayout = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2023ChargedUp.m_resourceFile);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        leftLowPoseEstimator = new PhotonPoseEstimator(
-            aprilTagFieldLayout, PoseStrategy.MULTI_TAG_PNP,
-            leftLowCam, leftLowRobotToCamera);
-
-        rightLowPoseEstimator = new PhotonPoseEstimator(
-            aprilTagFieldLayout, PoseStrategy.MULTI_TAG_PNP,
-            rightLowCam, rightLowRobotToCamera);
-
-        // log_leftRobotToCam.accept(AdvantageScopeUtils.toDoubleArr(leftLowRobotToCamera));
-        // log_rightRobotToCam.accept(AdvantageScopeUtils.toDoubleArr(rightLowRobotToCamera));
-
-    }
-
     public void periodic() {
     }
 
-    /**
-     * 
-     * @param prevEstimatedRobotPose
-     * @return an EstimatedRobotPose which includes a Pose3d of the latest estimated
-     *         pose (using the selected strategy) along with a double of the
-     *         timestamp when the robot pose was estimated
-     *         Use this to update drivetrain pose estimator
-     */
-    // public ArrayList<Optional<EstimatedRobotPose>> getEstimatedGlobalPose(Pose2d prevEstimatedRobotPose) {
-    //     ArrayList<Optional<EstimatedRobotPose>> estimatedGlobalPoses = new ArrayList<>();
-    //     poseEstimator1.setReferencePose(prevEstimatedRobotPose);
-    //     poseEstimator1.setLastPose(prevEstimatedRobotPose);
-    //     estimatedGlobalPoses.add( poseEstimator1.update());
-        
-    //     poseEstimator2.setReferencePose(prevEstimatedRobotPose);
-    //     poseEstimator2.setLastPose(prevEstimatedRobotPose);
-    //     estimatedGlobalPoses.add( poseEstimator2.update());
-
-    //     return estimatedGlobalPoses;
-    // }
-
-    public Optional<EstimatedRobotPose> leftLow_getEstPose(Pose2d prevEstimatedRobotPose) {
-        leftLowPoseEstimator.setReferencePose(prevEstimatedRobotPose);
-        leftLowPoseEstimator.setLastPose(prevEstimatedRobotPose);
-        return leftLowPoseEstimator.update();
-    }
-    
-    public Optional<EstimatedRobotPose> rightLow_getEstPose(Pose2d prevEstimatedRobotPose) {
-        rightLowPoseEstimator.setReferencePose(prevEstimatedRobotPose);
-        rightLowPoseEstimator.setLastPose(prevEstimatedRobotPose);
-        return rightLowPoseEstimator.update();
-    }
 
     // unfiltered view of camera
     public void setDriverMode(boolean driverMode) {
---
+++ src/main/java/frc/robot/vision/PhotonRunner.java
@@ -0,0 +1,67 @@
+package frc.robot.vision;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+import javax.swing.text.html.Option;
+
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.networktables.DoublePublisher;
+import edu.wpi.first.wpilibj.Notifier;
+import edu.wpi.first.wpilibj.Timer;
+import frc.lib.WaltLogger;
+
+
+public class PhotonRunner {
+    private final Notifier camNotifier = new Notifier(this::run);
+    private final PhotonCamera camera;
+    private final Transform3d robotToCamTransform;
+    private final PhotonPoseEstimator poseEstimator;
+    private final Supplier<Pose2d> robotPoseSupplier;
+
+    private Optional<EstimatedRobotPose> latestEstPose = Optional.empty();
+
+    private final DoublePublisher log_estTimeSec;
+
+    public PhotonRunner(
+        String cameraName,
+        Transform3d robotToCam,
+        AprilTagFieldLayout atfl,
+        Supplier<Pose2d> robotPoseSup
+    ) {
+        camera = new PhotonCamera(cameraName);
+        robotToCamTransform = robotToCam;
+        poseEstimator = new PhotonPoseEstimator(atfl, PoseStrategy.MULTI_TAG_PNP, camera, robotToCamTransform);
+        camNotifier.startPeriodic(0.02);
+        robotPoseSupplier = robotPoseSup;
+
+        final String entryPrefix = "PhotonRunner/" + cameraName + "/";
+
+        log_estTimeSec = WaltLogger.makeDoubleTracePub(entryPrefix + "estTimeSec");
+    }
+
+    private void run() {
+        var estBegin = Timer.getFPGATimestamp();
+        latestEstPose = getEstimatedPose(robotPoseSupplier.get());
+        var estElapsed = Timer.getFPGATimestamp() - estBegin;
+
+        log_estTimeSec.accept(estElapsed);
+    }
+
+    private Optional<EstimatedRobotPose> getEstimatedPose(Pose2d prevEstRobotPose) {
+        poseEstimator.setReferencePose(prevEstRobotPose);
+        poseEstimator.setLastPose(prevEstRobotPose);
+        return poseEstimator.update();
+    }
+
+    public Optional<EstimatedRobotPose> getLatestEstimatedPose() {
+        return latestEstPose;
+    }
+}
