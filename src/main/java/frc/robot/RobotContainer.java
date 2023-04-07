d2e42b01f7a65b09c041af608bdd11d56c81b98a
Oscar Chen <oscar1149412@gmail.com>
2023-04-07 10:23:36 -0400

    balance led

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -99,7 +99,7 @@ public class RobotContainer {
 		/* Driver Buttons */
 		driver.back().onTrue(swerve.teleOpReset());
 		driver.start().onTrue(new InstantCommand(() -> swerve.resetToAbsolute()));
-		driver.leftBumper().whileTrue(swerve.nowItsTimeToGetFunky()); // TODO: change reverse condition
+		driver.leftBumper().whileTrue(leds.setBalanced()); 
 		driver.rightBumper().onTrue(swerve.stopWithXCmd());
 
 			if(DriverStation.getAlliance().equals(Alliance.Blue)) {
