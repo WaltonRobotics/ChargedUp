f8caf490b4d2cb4323cde8607fee4c955c682c7b
graciexie <xiegrace61@gmail.com>
2023-04-11 19:40:10 -0400

    revert 2 bumpy

--- src/main/java/frc/robot/auton/Paths.java
+++ src/main/java/frc/robot/auton/Paths.java
@@ -117,7 +117,7 @@ public class Paths {
 				1.5);
 		public static final PathPlannerTrajectory oneConeBump = PathPlanner.loadPath("oneConeBump", kPPConstraints);
 
-		public static final List<PathPlannerTrajectory> twoPointFiveBumpy = PathPlanner.loadPathGroup("twoPointFiveBumpy", new PathConstraints(2, 1.5), kPPConstraints);
+		public static final PathPlannerTrajectory twoPointFiveBumpy = PathPlanner.loadPath("twoPointFiveBumpy", kPPConstraints);
 	}
 
 	public static final class PPAutoscoreClass {
